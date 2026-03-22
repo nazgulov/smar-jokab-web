559c02568e2045a5da334200be3c451a984f9110
Nazgul <koci.marek@icloud.com>
2026-03-22 07:20:30 +0100

    formular we3forms napojen

--- js/script.js
+++ js/script.js
@@ -7,13 +7,3 @@ if (menuToggle && nav) {
     });
 }
 
-const contactForm = document.getElementById("contactForm");
-const formMessage = document.getElementById("formMessage");
-
-if (contactForm && formMessage) {
-    contactForm.addEventListener("submit", function (event) {
-        event.preventDefault();
-        formMessage.textContent = "Děkujeme, zpráva byla připravena k odeslání.";
-        contactForm.reset();
-    });
-}
